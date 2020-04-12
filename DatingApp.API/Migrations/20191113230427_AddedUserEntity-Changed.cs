902501f72f22c32eeffbd01eaedb251549182432
Brijesh Patel <PatelB@ntpwarranty.com>
2020-04-12 16:17:15 -0400

    End of Section 3

---
+++ DatingApp.API/Migrations/20191113230427_AddedUserEntity-Changed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class AddedUserEntityChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "uerName",
+                table: "Users",
+                newName: "UerName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UerName",
+                table: "Users",
+                newName: "uerName");
+        }
+    }
+}
---
+++ DatingApp.API/Migrations/20200411231650_Change Username field.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class ChangeUsernamefield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UerName",
+                table: "Users",
+                newName: "UserName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Users",
+                newName: "UerName");
+        }
+    }
+}
