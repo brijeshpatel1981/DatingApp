6f57eaa579cee9bafa305d3358d30cc78022abd8
Brijesh Patel <PatelB@ntpwarranty.com>
2020-04-24 17:29:31 -0400

    End of Section 11

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
 
 
 @Injectable({
@@ -12,8 +15,15 @@ export class AuthService {
   baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
+  currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
 constructor(private http: HttpClient) { }
+
+changeMemberPhoto(photoUrl: string) {
+  this.photoUrl.next(photoUrl);
+}
 
 login(model: any) {
   return this.http.post(this.baseUrl + 'login', model)
@@ -22,7 +32,10 @@ login(model: any) {
       const resp = response;
       if (resp) {
         localStorage.setItem('token', resp.token);
+        localStorage.setItem('user', JSON.stringify(resp.user));
         this.decodedToken = this.jwtHelper.decodeToken(resp.token);
+        this.currentUser = resp.user;
+        this.changeMemberPhoto(this.currentUser.photoUrl);
       }
     })
   );
