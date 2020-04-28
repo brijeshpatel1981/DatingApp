06b8b65ede3bb510860305021a47bb9be3edceb9
Brijesh Patel <PatelB@ntpwarranty.com>
2020-04-28 12:26:08 -0400

    End of section 14

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/_models/user';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResults } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -11,20 +12,43 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
 users: User[];
+user: User = JSON.parse(localStorage.getItem('user'));
+genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+userParams: any = {};
+pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data.users;
+      this.users = data.users.results;
+      this.pagination = data.users.pagination;
     });
+
+    this.reserFilters();
   }
 
-  // loadusers() {
-  //   this.userService.getUsers().subscribe((users: User[]) => {
-  //     this.users = users;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadusers();
+  }
+
+  reserFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = "lastActive";
+
+    this.loadusers();
+  }
+
+  loadusers() {
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+    .subscribe((res: PaginatedResults<User[]>) => {
+      this.users = res.results;
+      this.pagination = res.pagination;
+    }, error => {
+      this.alertify.error(error);
+    });
+  }
 }
