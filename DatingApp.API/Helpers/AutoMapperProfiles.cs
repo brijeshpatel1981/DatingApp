b36b0ffdfb1140d7172d5d8775f4f6374033fdae
Brijesh Patel <PatelB@ntpwarranty.com>
2020-05-06 18:02:02 -0400

    End of Section 16

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -26,6 +26,15 @@ namespace DatingApp.API.Helpers
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
             CreateMap<UserForRegisterDto, User>();
+            CreateMap<MessageForCreationDto, Message>().ReverseMap();
+            CreateMap<Message, MessageToReturnDto>()
+                .ForMember(m => m.SenderPhotoUrl, opt =>
+                    opt.MapFrom(src => 
+                        src.Sender.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(m => m.RecipientPhotoUrl, opt =>
+                    opt.MapFrom(src => 
+                        src.Recipient.Photos.FirstOrDefault(p => p.IsMain).Url));
+                
         }
     }
 }
